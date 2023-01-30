56aa000dcd500ad84909ee320fe1459a6a5515e8
jp-walker <lwalker2677@scrippscollege.edu>
2023-01-30 15:04:13 -0800

    fixed flake8 errors

--- Fixme.py
+++ Fixme.py
@@ -5,6 +5,7 @@ Your solutions should use the map and filter functions,
 and not for loops or list comprehensions.
 '''
 
+
 def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
@@ -20,21 +21,24 @@ def evens(n):
     >>> evens(-1)
     [0]
     '''
-    
+
     result = []
+
     if n >= 0:
         numbers = list(range(n+1))
     else:
-        numbers = list(range(n,1))
+        numbers = list(range(n, 1))
     for i in numbers:
         if i % 2 == 0:
             result.append(i)
     return result
-            
+
 
 def threes(n):
     '''
-    Returns a list of all numbers from 0 to n inclusive that contain the digit 3.
+    Returns a list of all numbers from 0 to n inclusive
+    that contain the digit 3.
+
 
     >>> threes(2)
     []
@@ -47,16 +51,20 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
+
     result = []
-    for i in list(range(0,n+1)):
+
+    for i in list(range(0, n+1)):
         i = str(i)
         if '3' in i:
             result.append(int(i))
     return result
 
+
 def small_words(text):
     '''
-    Returns a list of all words in the input text that are less than 5 characters long.
+    Returns a list of all words in the input text that are less than 5
+    characters long.
 
     HINT:
     Recall that text.split() converts the text variable into a list of words.
@@ -69,13 +77,16 @@ def small_words(text):
     []
     >>> small_words('a big word is bad')
     ['a', 'big', 'word', 'is', 'bad']
-    ''' 
+    '''
+
     result = []
+
     text = text.split(" ")
     for i in text:
         if len(i) < 5 and i != "":
             result.append(i)
     return result
+
 
 def squares(n):
     '''
@@ -94,13 +105,15 @@ def squares(n):
 
     result = []
 
-    for i in list(range(1,n+1)):
+    for i in list(range(1, n+1)):
         result.append(i**2)
-    return result 
+    return result
+
 
 def lengths(strings):
     '''
-    Given a list of strings, returns a list of the lengths of the corresponding strings.
+    Given a list of strings, returns a list of the lengths of the
+    corresponding strings.
 
     >>> lengths([])
     []
